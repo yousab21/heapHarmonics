5d47f976872f89813419c1f59ec237be3dbb0e2b
omar <omarmashal1470@gmali.com>
2025-12-03 22:28:21 +0200

    abdel7y

---
+++ project/project/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        
+        message e1=new message(1,"OMAR"," IAM WORKING ON MY PROJECT","AHMED");
+        e1.getcontent();
+        e1.getreceiver( );
+        e1.getsender();
+        e1.getime();
+        e1.getmessageID();
+        e1.displaymessage();
+    }
+}
